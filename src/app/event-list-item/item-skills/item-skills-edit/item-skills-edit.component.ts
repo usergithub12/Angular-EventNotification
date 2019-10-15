d14e12c77944b3818913805eed2e7179cc583a11
Микола Мантицький <slwpke447@gmail.com>
2019-10-15 20:53:00 +0300

    add new skills

--- src/app/event-list-item/item-skills/item-skills-edit/item-skills-edit.component.ts
+++ src/app/event-list-item/item-skills/item-skills-edit/item-skills-edit.component.ts
@@ -1,26 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import {SKILLS} from '../../../mock-skills'
-import { Skill } from 'src/app/skill';
+import { Component, OnInit } from "@angular/core";
+import { SKILLS } from "../../../mock-skills";
+import { Skill } from "src/app/skill";
 @Component({
-  selector: 'app-item-skills-edit',
-  templateUrl: './item-skills-edit.component.html',
-  styleUrls: ['./item-skills-edit.component.scss']
+  selector: "app-item-skills-edit",
+  templateUrl: "./item-skills-edit.component.html",
+  styleUrls: ["./item-skills-edit.component.scss"]
 })
 export class ItemSkillsEditComponent implements OnInit {
+  skills = SKILLS;
 
-  skills=SKILLS;
-  
-  constructor() { }
+  skill: Skill = {
+    name: "",
+    level: ""
+  };
+  constructor() {}
 
   ngOnInit() {
-    console.log(this.skills)
+    console.log(this.skills);
   }
 
-   
-onSubmit(skill){
-console.log(skill);
-this.skills.push(new Skill(skill.skill,skill.level));
-} 
-
-
+  addSkill() {
+    this.skills.push(this.skill);
+  }
 }
